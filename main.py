a95ba5cad22b14d511d44830fe3b3e025ba0626c
Dylan K <dylan_kwok_heng_yi@s2023.ssts.edu.sg>
2025-06-12 16:06:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# aaa
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder, LabelEncoder
+
