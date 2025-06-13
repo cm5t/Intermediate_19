9620864217fcd0652a9f1256d780527e455486e2
Adithya <adithya181209@gmail.com>
2025-06-13 23:11:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ st.markdown(
     """
     Our group decided to create a Machine Learning Model that predicts the urgency of saving for families based on their income, expenditure and other data. The model is trained on a dataset that includes various financial metrics, such as household income, food expenditure, transportation costs, and more.
     ### We used a Random Forest Algorithm, with 100s of if-else statements to predict whether the family requires saving.
-    We also used Linear Regression.
     This can be applied in the Singapore Context, and worldwide as well!
     ### The saving tiers are as follows:
     ### 0: "✅ No urgency – You're doing great!",
