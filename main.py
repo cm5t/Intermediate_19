a8dd16f61e7d7c0564233cc75221d230d77014ba
Dylan K <dylan_kwok_heng_yi@s2023.ssts.edu.sg>
2025-06-13 11:41:49 +0800

    Added data + show head on streamlit

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder, LabelEncoder
+import streamlit as st
 
+
+data = pd.read_csv("buildingblocs25/Cleaned_Family_Data.csv")
+st.dataframe(data.head(5))
